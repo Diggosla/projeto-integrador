417ac41dcf06357ffa1022ae24177f3116bdf598
Diogo Zanetti <diogozanetti28@gmail.com>
2023-03-10 22:12:55 -0300

    arrumar-linha-2
    linha1 feita
    arrumar linha 2

---
+++ inicio/comeco/app.js
@@ -0,0 +1,33 @@
+
+
+controls.forEach((control) => {
+    control.addEventListener("click", (e) => {
+        isLeft = e.target.classList.contains("arrow-left");
+
+        if (isLeft) {
+            currentItem -= 1;
+        }
+        else {
+            currentItem += 1;
+        }
+
+        if (currentItem >= maxItems) {
+            currentItem = 0;
+        }
+        if (currentItem < 0) {
+            currentItem = maxItems - 1;
+        }
+        items.forEach((item) => item.classList.remove("current-item"));
+
+        items[currentItem].scrollIntoView
+            ({
+                behavior: "smooth",
+                inline: "center",
+                block: "nearest"
+            });
+
+        items[currentItem].classList.add("current-item");
+    });
+});
+
+
